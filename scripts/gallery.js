4f51dfb6edc296bf95dbbc016cbb618d173f9e23
Levi Jost martins <levijostmartins@Levis-MacBook-Pro.local>
2024-07-26 01:17:17 -0300

    final project kidna done. I dont have to fix anything else, please bring 14 weeks classes again

---
+++ scripts/gallery.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const photos = [
+      'images/wed1.jpg',
+      'images/wed2.png',
+      'images/bw.jpg',
+      'images/wed4.png',
+      'images/wed3.jpg',
+      'images/piano1.jpg',
+      'images/enquadrado.jpg',
+      'images/plant2.jpg',
+
+  ];
+
+  const galleryContainer = document.querySelector('.gallery-container');
+
+  photos.forEach(photo => {
+      const imgElement = document.createElement('img');
+      imgElement.src = photo;
+      imgElement.classList.add('gallery-photo');
+      imgElement.addEventListener('click', openModal);
+      galleryContainer.appendChild(imgElement);
+  });
+
+  const modal = document.getElementById('modal');
+  const modalImg = document.getElementById('modal-img');
+  const closeBtn = document.querySelector('.close');
+
+  function openModal(event) {
+      modal.style.display = 'block';
+      modalImg.src = event.target.src;
+  }
+
+  closeBtn.onclick = function() {
+      modal.style.display = 'none';
+  }
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = 'none';
+      }
+  }
+});
